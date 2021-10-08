22832549e1ae4573aa3f043dc0e168e044bf8855
Josmarhdez <89487425+Josmarhdez@users.noreply.github.com>
2021-10-08 11:25:09 -0500

    Update App.java

--- ProyectoLogeo/src/main/java/mx/uv/App.java
+++ ProyectoLogeo/src/main/java/mx/uv/App.java
@@ -1,13 +1,42 @@
 package mx.uv;
 
-/**
- * Hello world!
- *
- */
+import static spark.Spark.*;
+import com.google.gson.*;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
+
+        get("/saludar", (req, res) -> {
+            String l = req.queryParams("usuario");
+            String p = req.queryParams("password");
+            String respuesta;
+            System.out.println(l + " " + p);
+
+            if (l.equals("Usuario1") && p.equals("123456"))
+                respuesta = "Bienvenido usuario ";
+            else
+                respuesta = "Usuario equivocado ";
+            return respuesta + l + " <a href='http://127.0.0.1:5500/envio_formulario.html'>volver</a>";
+        });
+    
+
     }
 }
